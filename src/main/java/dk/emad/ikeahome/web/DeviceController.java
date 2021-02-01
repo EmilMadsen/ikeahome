d2d36a0c0b631954c0dbc33bf8e1e71de1ea58ed
EmilMadsen <emilmadsen1993@gmail.com>
2021-02-01 21:15:48 +0100

    logging

--- src/main/java/dk/emad/ikeahome/web/DeviceController.java
+++ src/main/java/dk/emad/ikeahome/web/DeviceController.java
@@ -3,6 +3,8 @@ package dk.emad.ikeahome.web;
 import dk.emad.ikeahome.service.DeviceService;
 import nl.stijngroenen.tradfri.device.Device;
 import nl.stijngroenen.tradfri.device.Light;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +15,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api")
 public class DeviceController {
+    private static final Logger log = LoggerFactory.getLogger(DeviceController.class);
 
     private final DeviceService deviceService;
 
@@ -22,11 +25,13 @@ public class DeviceController {
 
     @GetMapping("/id/{id}")
     public Device getById(@PathVariable int id) {
+        log.info("getById: {}", id);
         return deviceService.getById(id);
     }
 
     @GetMapping("/id/{id}/on/{on}/brightness/{brightness}")
     public Light setById(@PathVariable int id, @PathVariable boolean on, @PathVariable int brightness) {
+        log.info("setById - id: {} - on: {} - brightness: {}", id, on, brightness);
         return deviceService.setById(id, on, brightness);
     }
 
@@ -34,11 +39,13 @@ public class DeviceController {
 
     @GetMapping("/name/{name}")
     public List<Device> getByName(@PathVariable String name) {
+        log.info("getByName: {}", name);
         return deviceService.getByName(name);
     }
 
     @GetMapping("/name/{name}/on/{on}/brightness/{brightness}")
     public List<Device> setByName(@PathVariable String name, @PathVariable boolean on, @PathVariable int brightness) {
+        log.info("setByName - name: {} - on: {} - brightness: {}", name, on, brightness);
         return deviceService.setByName(name, on, brightness);
     }
 
@@ -46,11 +53,13 @@ public class DeviceController {
 
     @GetMapping("/all")
     public Device[] getAll() {
+        log.info("getAll");
         return deviceService.getAll();
     }
 
     @GetMapping("/all/on/{on}/brightness/{brightness}")
     public Device[] setAll(@PathVariable boolean on, @PathVariable int brightness) {
+        log.info("setAll - on: {} - brightness: {}", on, brightness);
         return deviceService.setAll(on, brightness);
     }
 
